0d690596d4277fbfb23a3f67b25486aaa997a6ba
christhielen <github@sandgnat.com>
2015-11-12 15:46:44 -0600

    add scripts

---
+++ data/fetch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -o messages.txt http://beta.json-generator.com/api/json/get/VJl5GbIze
+curl -o contacts.json http://beta.json-generator.com/api/json/get/V1g6UwwGx
---
+++ data/generate.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+./fetch.sh
+curl -O https://raw.githubusercontent.com/banksean/jsmarkov/master/markov.js
+curl -O https://raw.githubusercontent.com/banksean/jsmarkov/master/probabilityset.js
+
+gunzip -k corpora/*.gz
+
+cat probabilityset.js  >> markov.js
+echo >> markov.js
+echo "exports.Markov = Markov;" >> markov.js
+
+node generate
+
+rm markov.js probabilityset.js
